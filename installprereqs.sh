a587c3a16d7fdbeb292fc28958f8d2de704dae04
ambarish2012 <achitnis2012@gmail.com>
2018-01-06 12:10:27 -0800

    Create installprereqs.sh

---
+++ installprereqs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Prerequisites
+sudo apt-get install --assume-yes jq
+
+# Get URLs for most recent versions
+terraform_url=$(curl https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*amd64" | sort --version-sort -r | head -1 | awk -F[\"] '{print $4}')
+
+# Create a move into directory.
+mkdir terraform && cd $_
+
+# Download Terraform. URI: https://www.terraform.io/downloads.html
+echo "Downloading $terraform_url."
+curl -o terraform.zip $terraform_url
+# Unzip and install
+unzip terraform.zip
+
+
+echo '
+# Terraform path.
+export PATH=~/terraform/:$PATH
+' >>~/.bashrc
+
+source ~/.bashrc
+terraform version
+
